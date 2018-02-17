7fa9f4af469ce5733d86b6ea9d0a7952b88feb19
harishkrishnan24 <harishkrishnan1993@gmail.com>
2018-02-17 15:54:43 +0530

    Added mongoDB database support to save user details

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var jwt = require('jwt-simple');
 var _ = require('lodash');
 var bcrypt = require('bcrypt');
 var app = express();
+var User = require('./user');
 
 app.use(require('body-parser').json());
 
@@ -23,23 +24,55 @@ function validateUser(user, password, cb) {
     return bcrypt.compareSync(password, user.password, cb);
 }
 
-app.post('/session', function (req, res) {
-    var user = findUserByUsername(req.body.username);
-    validateUser(user, req.body.password, function (err, valid) {
-        if (err || !valid) {
+app.post('/session', function (req, res, next) {
+    User.findOne({
+        username: req.body.username
+    }).select('passwod').exec(function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
             return res.send(401);
         }
-        var token = jwt.encode({
-            username: username
-        }, secretKey);
-        res.json(token);
+        bcrypt.compare(req.body.password, user.password, function (err, valid) {
+            if (err) {
+                return next(err);
+            }
+            if (!valid) {
+                return res.send(410);
+            }
+            var token = jwt.encode({
+                username: user.username
+            }, secretKey);
+            res.json(token);
+        });
     });
 });
 
 app.get('/user', function (req, res) {
     var token = req.headers['x-auth'];
-    var user = jwt.decode(token, secretKey);
+    var auth = jwt.decode(token, secretKey);
+    User.findOne({
+        username: auth.username
+    }, function (err, user) {
+        res.json(user);
+    });
     res.json(user);
+});
+
+app.post('/user', function (req, res, next) {
+    var user = new User({
+        username: req.body.username
+    });
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        user.password = hash;
+        user.save(function (err, user) {
+            if (err) {
+                throw next(err);
+            }
+            res.send(201);
+        });
+    });
 });
 
 app.listen(3000, function () {
